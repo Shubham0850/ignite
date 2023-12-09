19c913faf5a8fc0879fdc580a97a735a6f615807
sahilkakwani9 <sahkakwani@gmail.com>
2023-12-09 23:34:33 +0530

    util: profile and metadata upload

--- config/client.ts
+++ config/client.ts
@@ -1,5 +1,9 @@
-import {baseGoerli} from 'viem/chains';
+import { base, baseGoerli } from "viem/chains";
 
-export const chain = baseGoerli;
-export const alchemyApiKey = '-i3pq4S767V73GFdK_OtN-lXphqcT267';
-export const publishableKey = 'pk_live_BCB248E5DDBC031F';
+export const chain = base;
+export const alchemyApiKey = "-i3pq4S767V73GFdK_OtN-lXphqcT267";
+export const publishableKey = "pk_live_BCB248E5DDBC031F";
+export const lightHouseApiKey = "cc54bc1a.548fbe3ce7ca412c92e66a7471981d31";
+export const IPFS_UPLOAD_API = "https://api.web3.storage/upload";
+export const WEB3STORAGETOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDMxQjM3MjRlN2E2RjgyMTg3NEFEYzMyYjEzZDQwNjM2YkFiMDA0NzEiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NTg0MTc0ODY2ODksIm5hbWUiOiJDYXplIn0.BoGp0mNQc_syBHBZFj6Mb2dTUjAlpLWHd6DwCwFSc_4";
